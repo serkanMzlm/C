2562422860aa87f53e9d6f46479afe1896fe293f
serkanMzlm <74418302+serkanMzlm@users.noreply.github.com>
2021-03-27 22:11:56 +0300

    Update and rename while_dowhile.c to 1.05_while_dowhile.c

--- syntax/1.05_while_dowhile.c
+++ syntax/1.05_while_dowhile.c
@@ -4,14 +4,14 @@ int main(){
 	int deger;
 printf("enter a value :\n");
 	scanf("%d",&deger);
-		while(deger<5){
+		while(deger<5){ //kosul saglandıgı sürece dongü devam eder
 		printf("entered value = %d\n ",deger);
 	
 	 deger++;
 	}
 	printf("while failed \n");
 	printf("---------> do while ");
-	do{ 
+	do{ //while farki once döngüye girer sonra kontrol eder
 	printf("\nentered value = %d",deger);
 	deger ++;	
 	}while(deger<5);
